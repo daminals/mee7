ac7c2119f13e2eaca46c7526e1d3853b0b69a189
Daniel Kogan <daniel@techhacks.nyc>
2020-05-02 14:00:06 -0400

    ready?

--- bot.py
+++ bot.py
@@ -5,38 +5,30 @@ import discord, random, asyncio
 from dotenv import load_dotenv
 from discord import Member
 from itertools import cycle
+from firebase import firebase
 
 load_dotenv()
 from discord.ext import commands, tasks
 
 bot = commands.Bot(command_prefix='!', description='Aejay\'s Birthday!!')
 TOKEN = os.environ.get('TOKEN', 3)
+FIREBASE = os.environ.get('FIREBASE', 3)
+FIREBASE_NAME = os.environ.get('FIREBASE_NAME', 3)
+
+MEE6 = bot.get_user(159985870458322944)
+firebase = firebase.FirebaseApplication(FIREBASE, None)
+
+ListOfMEE6insults = []
+
 
 
 @bot.event
 async def on_ready():
-    party = bot.get_emoji(698725283649290310)
-    bot_channel = bot.get_channel(689168363083268249)
-    user = bot.get_user(340996105460514816)
-    # 340996105460514816 - Aejay
-    # 577668867380477962 - me
-    background_task.start(user, bot_channel)
-    await bot.change_presence(
-        activity=discord.Activity(type=discord.ActivityType.watching, name="Aejay... Constantly", emoji=party))
     print('bot.py is active')
-
-
-global_speed = 60*15
 
 
 @bot.event
 async def on_message(message):
-    if message.channel == bot.get_channel(689168363083268249):
-        emoji = '🥳'
-        emoji2 = '🎉'
-        await message.add_reaction(emoji)
-        await message.add_reaction(emoji2)
-    else:
         if random.randint(0, 100) > 99:
             emoji = '🥳'
             emoji2 = '🎉'
@@ -50,38 +42,20 @@ async def on_message(message):
             if not message.author.bot:
                 await message.add_reaction("🤡")
                 await message.channel.send('LMAO SIMP!!')
-
-
-@bot.command(name='speed')
-async def speed(ctx, speed):
-    await bot.wait_until_ready()
-    await ctx.channel.send('lmfao i have no clue why im bbroken teehee')
-    if ctx.message.author == bot.get_user(340996105460514816):
-        await ctx.channel.send(f'{bot.get_user(340996105460514816)} cannot use this command')
-        return
-    global global_speed
-    global_speed = speed
-    await ctx.send(f'speed updated to {speed}')
-
-
-@tasks.loop(seconds=global_speed)
-async def background_task(user, bot_channel):
-    await bot.wait_until_ready()
-    await bot_channel.send(f'Happy Birthday {user.mention}! 🥳🎉')
-    print('happy brithday')
-
-
-@bot.command(name='tracking')
-async def tracking(ctx):
-    embed = discord.Embed(title='Tracking:', description=f'{global_speed}')
-    await ctx.channel.send(embed=embed)
-
+        if message.author == MEE6:
+            await message.channel.send()
 
 @bot.command(hidden=True)
 async def load(ctx):
     for filename in os.listdir('./cogs'):
         if filename.endswith('.py'):
             bot.load_extension(f'cogs.{filename[:-3]}')
+
+
+@bot.command(name='insult')
+async def insult(ctx, *, insult):
+    result = firebase.post(FIREBASE_NAME+'/insult', insult)
+    print(result)
 
 
 @bot.command(hidden=True)
