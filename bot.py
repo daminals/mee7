413993663114817eae87278969a682f65463e9c1
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2020-11-11 13:44:39 -0500

    dict not callable silly, have to use []

--- bot.py
+++ bot.py
@@ -183,7 +183,7 @@ async def on_message(message):
     for i in banned:
         if i in message.content.lower():
             await message.delete()
-        if CENSOR_DICT(Server):
+        if CENSOR_DICT[Server]:
             await message.channel.send(f'*{i}* is banned please shut the f@@k up already {message.author.mention}')
         else:
             await message.channel.send(f'*{i}* is banned please shut the fuck up already {message.author.mention}')
