9cd02cb638001cfc432a0d0c5442d04a0e7a2831
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2021-03-28 18:00:34 -0400

    dontdownvoteme

--- bot.py
+++ bot.py
@@ -141,13 +141,13 @@ async def on_reaction_add(reaction,user):
 """    
 @bot.event
 async def on_reaction_add(reaction,user):
-    if reaction.message.guild != bot.get_guild(706202537434284083):
-        return
     mee7 = bot.get_user(706194661366300753)
     me = bot.get_user(577668867380477962)
-    await reaction.message.reply(reaction.emoji)
-    if reaction.emoji == '<:downvote:776162465842200617>':
-        if user != mee7: #or user != me:
+    downvote = bot.get_emoji(776162465842200617) # '<:downvote:776162465842200617>'
+    #await reaction.message.reply(reaction.emoji)
+    if reaction.emoji == downvote:
+        #await reaction.message.channel.send("bruh")
+        if user != mee7 and user != me:
             if reaction.message.author == me:
                 await reaction.remove(user)
 
