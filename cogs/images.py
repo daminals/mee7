3d2db1974a75e7936ecc3200904ebf6f9f0f6445
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2021-05-02 20:49:01 -0400

    import_didnt_work in the cog

--- cogs/images.py
+++ cogs/images.py
@@ -1,18 +1,79 @@
 #images.py
-import discord,time,random,sys,os, requests,shutil
+import discord,time,random,sys,os, os,ffmpy,moviepy.editor, requests,shutil
 from discord.ext import commands
 from discord import Member
 from PIL import Image, ImageFilter, ImageFont, ImageDraw
-from random import randint
+from random import randint, random, uniform
 
 import colorama, moviepy
 from colorama import Fore
 from colorama import Style
 
-from cogs.video import *
-
 sys.path.append(os.path.abspath('../'))
 from bot import firebase, FIREBASE_NAME
+
+
+def add_topv(vid, caption, Fw, h):
+    newH = h * 1.35
+    placementH = h*0.35
+    font_size = int(h * 0.1)
+    caption = fit_text(caption, Fw, font_size)
+    
+    ff = ffmpy.FFmpeg(
+     inputs={vid: None},
+     outputs={f'static/created/captioned.mp4': f'-vf "pad=iw:{newH}:iw/2:{placementH}:color=white",drawtext="fontfile=static/fonts/impact.ttf":text="{caption}":fontcolor=black:fontsize={font_size}:x=(w-tw)/2:y=({placementH}-th)/2:fix_bounds=true:line_spacing=3 -vcodec libx264 -codec:a copy -crf 35'}
+    )
+    ff.run()
+
+def fit_text(string: str, frame_width, font_size):
+    split_line = [x.strip() for x in string.split()]
+    translation_font = ImageFont.truetype("static/fonts/impact.ttf", size=font_size, encoding="unic")
+    lines = ""
+    w = 0
+    line_num = 0
+    line = ""
+    for word in split_line:
+        # Make a test
+        w, _ = translation_font.getsize(" ".join([line, word]))
+        # If it exceeds the frame width, add a new line
+        if w > (frame_width - (2 * 4)):  # Leave 6px margin on each side
+            lines += line.strip() + "\n"
+            line = ""
+
+        line += word + " "
+
+    lines += line.strip()  # Append leftover words
+    return lines
+
+def deepfryv(vid, repeat):
+    ff = ffmpy.FFmpeg(
+     inputs={vid: None},
+     outputs={f'static/created/deepfried{repeat+1}.mp4': f'{create_filter_args()} {create_audio_args(repeat)}-vcodec libx264 -crf 45'}
+    )
+    ff.run()
+
+def create_audio_args(repeat):
+    return '-af ' + 'bass=g=18,treble=g=2,volume=10dB,' + 'acompressor=threshold=0.02:makeup=5,acontrast=45 '
+
+def create_filter_args():
+    """
+    Create randomized "deep fried" visual filters
+    returns command line args for ffmpeg's filter flag -vf
+    """
+    saturation = uniform(2, 5)
+    contrast = uniform(1, 8)
+    noise = uniform(5, 20)
+    gamma_r = uniform(2, 3)
+    gamma_g = uniform(1, 2)
+    gamma_b = uniform(1, 1.5)
+
+    eq_str = 'eq=saturation={}:contrast={}'.format(saturation, contrast)
+    eq_str += ':gamma_r={}:gamma_g={}:gamma_b={}'.format(gamma_r, gamma_g, gamma_b)
+    noise_str = 'noise=alls={}:allf=t'.format(noise)
+    sharpness_str = 'unsharp=3:3:1.3:3:3:1.5'
+
+    return '-vf ' + eq_str + ',' + noise_str + ',' + sharpness_str
+
 
 
 def download_link(referenced, filename):
