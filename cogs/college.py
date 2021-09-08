4bdb90a1c9072740576742c4bbf0bfd0f46d0408
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2021-09-08 17:45:54 -0400

    unbrike

--- cogs/college.py
+++ cogs/college.py
@@ -18,13 +18,14 @@ class College(commands.Cog):
 
     @commands.command(name='ethos')
     async def ethos(self, ctx):
-        await ctx.add_reaction("✅")
+        await ctx.message.add_reaction("✅")
         ans = "the author's character, manifested by his intelligence, confidence, trustworthiness, knowledge of the subject, and respect for the reader"
         for i in range(3):
             def check(m):
                 return ctx.author == m.author #To make sure it is the only message author is getting
             msg = await self.bot.wait_for('message', timeout=90.0, check=check)
             if msg.content == ans:
+                await msg.add_reaction("✅")
                 await msg.reply("Correct✅")
                 await msg.delete()
                 return
@@ -35,13 +36,14 @@ class College(commands.Cog):
  
     @commands.command(name='pathos')
     async def pathos(self, ctx):
-        await ctx.add_reaction("✅")
+        await ctx.message.add_reaction("✅")
         ans = "evoking of emotions in the reader and not irritating the reader with poor punctuation or grammar; it can include emotional responses the author may not have expected"
         for i in range(3):
             def check(m):
                 return ctx.author == m.author #To make sure it is the only message author is getting
             msg = await self.bot.wait_for('message', timeout=90.0, check=check)
             if msg.content == ans:
+                await msg.add_reaction("✅")
                 await msg.reply("Correct✅")
                 await msg.delete()
                 return
@@ -53,13 +55,14 @@ class College(commands.Cog):
 
     @commands.command(name='logos')
     async def logos(self, ctx):
-        await ctx.add_reaction("✅")
+        await ctx.message.add_reaction("✅")
         ans = "reasoning, that can include examples, facts, statistics, syllogisms, or statements from authoritative figures to draw conclusions"
         for i in range(3):
             def check(m):
                 return ctx.author == m.author #To make sure it is the only message author is getting
             msg = await self.bot.wait_for('message', timeout=90.0, check=check)
             if msg.content == ans:
+                await msg.add_reaction("✅")
                 await msg.reply("Correct✅")
                 await msg.delete()
                 return
