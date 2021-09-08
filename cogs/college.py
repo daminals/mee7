2c79cddc5c4057e23175da852b8c0fad745c1972
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2021-09-08 11:22:30 -0400

    check

--- cogs/college.py
+++ cogs/college.py
@@ -18,6 +18,7 @@ class College(commands.Cog):
 
     @commands.command(name='ethos')
     async def ethos(self, ctx):
+        await ctx.add_reaction("✅")
         ans = "the author's character, manifested by his intelligence, confidence, trustworthiness, knowledge of the subject, and respect for the reader"
         for i in range(3):
             def check(m):
@@ -35,6 +36,7 @@ class College(commands.Cog):
  
     @commands.command(name='pathos')
     async def pathos(self, ctx):
+        await ctx.add_reaction("✅")
         ans = "evoking of emotions in the reader and not irritating the reader with poor punctuation or grammar; it can include emotional responses the author may not have expected"
         for i in range(3):
             def check(m):
@@ -53,6 +55,7 @@ class College(commands.Cog):
 
     @commands.command(name='logos')
     async def logos(self, ctx):
+        await ctx.add_reaction("✅")
         ans = "reasoning, that can include examples, facts, statistics, syllogisms, or statements from authoritative figures to draw conclusions"
         for i in range(3):
             def check(m):
