bc53d6098fb51b7b87bac472a5612038e3b6108c
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2021-09-09 08:08:32 -0400

    amogus

--- cogs/college.py
+++ cogs/college.py
@@ -20,36 +20,34 @@ class College(commands.Cog):
     async def ethos(self, ctx):
         await ctx.message.add_reaction("✅")
         ans = "the author's character, manifested by his intelligence, confidence, trustworthiness, knowledge of the subject, and respect for the reader"
-        for i in range(3):
-            def check(m):
-                return ctx.author == m.author #To make sure it is the only message author is getting
-            msg = await self.bot.wait_for('message', timeout=90.0, check=check)
-            if msg.content == ans:
-                await msg.add_reaction("✅")
-                await msg.reply("Correct✅")
-                await msg.delete()
-                return
-            else:
-                await msg.reply("Incorrect. Try again")
-                print(ans)
+        def check(m):
+            return ctx.author == m.author #To make sure it is the only message author is getting
+        msg = await self.bot.wait_for('message', timeout=90.0, check=check)
+        if msg.content == ans:
+            await msg.add_reaction("✅")
+            await msg.reply("Correct✅")
+            await msg.delete()
+            return
+        else:
+            await msg.reply("Incorrect. Try again")
+            await ctx.send(ans)
                 
  
     @commands.command(name='pathos')
     async def pathos(self, ctx):
         await ctx.message.add_reaction("✅")
         ans = "evoking of emotions in the reader and not irritating the reader with poor punctuation or grammar; it can include emotional responses the author may not have expected"
-        for i in range(3):
-            def check(m):
-                return ctx.author == m.author #To make sure it is the only message author is getting
-            msg = await self.bot.wait_for('message', timeout=90.0, check=check)
-            if msg.content == ans:
-                await msg.add_reaction("✅")
-                await msg.reply("Correct✅")
-                await msg.delete()
-                return
-            else:
-                await msg.reply("Incorrect. Try again")
-                print(ans)
+        def check(m):
+            return ctx.author == m.author #To make sure it is the only message author is getting
+        msg = await self.bot.wait_for('message', timeout=90.0, check=check)
+        if msg.content == ans:
+            await msg.add_reaction("✅")
+            await msg.reply("Correct✅")
+            await msg.delete()
+            return
+        else:
+            await msg.reply("Incorrect. Try again")
+            await ctx.send(ans)
 
 
 
@@ -57,18 +55,17 @@ class College(commands.Cog):
     async def logos(self, ctx):
         await ctx.message.add_reaction("✅")
         ans = "reasoning, that can include examples, facts, statistics, syllogisms, or statements from authoritative figures to draw conclusions"
-        for i in range(3):
-            def check(m):
-                return ctx.author == m.author #To make sure it is the only message author is getting
-            msg = await self.bot.wait_for('message', timeout=90.0, check=check)
-            if msg.content == ans:
-                await msg.add_reaction("✅")
-                await msg.reply("Correct✅")
-                await msg.delete()
-                return
-            else:
-                await msg.reply("Incorrect. Try again")
-                print(ans)
+        def check(m):
+            return ctx.author == m.author #To make sure it is the only message author is getting
+        msg = await self.bot.wait_for('message', timeout=90.0, check=check)
+        if msg.content == ans:
+            await msg.add_reaction("✅")
+            await msg.reply("Correct✅")
+            await msg.delete()
+            return
+        else:
+            await msg.reply("Incorrect. Try again")
+            await ctx.send(ans)
            
 
 def setup(bot):
